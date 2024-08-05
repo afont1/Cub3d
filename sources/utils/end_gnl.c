95dd123b686fe288716b831bd4b14312682759db
afont1 <afont@student.42nice.fr>
2024-08-05 10:34:06 +0200

    norminette

--- sources/utils/end_gnl.c
+++ sources/utils/end_gnl.c
@@ -6,7 +6,7 @@
 /*   By: afont <afont@student.42nice.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 10:17:02 by afont             #+#    #+#             */
-/*   Updated: 2024/08/05 10:18:42 by afont            ###   ########.fr       */
+/*   Updated: 2024/08/05 10:33:38 by afont            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	ft_end_gnl(int fd)
 {
-	char *line;
-	
+	char	*line;
+
 	while (1)
 	{
 		line = get_next_line(fd);
